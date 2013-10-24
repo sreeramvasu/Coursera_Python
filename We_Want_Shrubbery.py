9e9d274a0e134d3769579294af414c233a482010
sreeramvasu <sreeram.vasudevan@gmail.com>
2013-10-24 15:30:32 +0800

    Create We_Want_Shrubbery.py
    First Mini Project

---
+++ We_Want_Shrubbery.py
@@ -0,0 +1,8 @@
+# mini-project 0 
+# Author: Sreeram Vasudevan
+# MailTo: sreeram.vasudevan@gmail.com
+
+# Modify the print statement according to 
+# the mini-project instructions
+
+print "We want... a shrubbery!"
